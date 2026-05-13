96e20b970c8cf7fff3abb5532aec282259b2d65a
dyjng0 <david.j.inbox@outlook.com>
2026-05-12 21:41:23 -0400

    Create getECBScore function

--- src/lib/cryptanalysis.hpp
+++ src/lib/cryptanalysis.hpp
@@ -16,3 +16,5 @@ double avgHammingDist(std::span<const uint8_t> buffer, size_t keySize);
 size_t findKeySize(std::span<const uint8_t> buffer);
 std::vector<uint8_t> breakVigenere(std::span<const uint8_t> buffer,
                                    size_t keySize);
+
+int getECBScore(std::span<const uint8_t> buffer);
