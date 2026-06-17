6e17167ef3a7e976daf7e1d32c4a0821031bc217
dyjng0 <david.j.inbox@outlook.com>
2026-06-17 01:04:28 -0400

    Add decryption logic

--- src/challenges/2-12_decryptECB.cpp
+++ src/challenges/2-12_decryptECB.cpp
@@ -1,6 +1,5 @@
 #include <cstdint>
 #include <iostream>
-#include <string>
 #include <vector>
 
 #include "src/challenges/challenges.hpp"
@@ -9,22 +8,21 @@
 #include "src/lib/oracles.hpp"
 
 int challenge2_12(int argc, char **argv) {
-  std::string input = "";
-  if (argc == 1) {
-    input = "Yerr";
-  } else if (argc == 2) {
-    input = argv[1];
-  } else {
-    std::cerr << "Usage ./cryptopals 2-12  OR ./cryptopals <base64>\n";
-    return 1;
+  if (argc == 2) {
+    auto buffer = base64ToBytes(argv[1]);
+    if (!buffer) {
+      std::cerr << "Error: invalid base64.\n";
+      return 1;
+    }
+    std::vector<uint8_t> ciphertext = ecbEncryptionOracle(*buffer);
+    std::cout << "Ciphertext: " << bytesToHex(ciphertext) << '\n';
   }
-  auto buffer = base64ToBytes(input);
-  if (!buffer) {
-    std::cerr << "Error: invalid base64.\n";
+  if (argc > 2) {
+    std::cerr << "Usage ./cryptopals 2-12 OR ./cryptopals 2-12 <base64>\n";
     return 1;
   }
 
-  size_t blockSize = findBlockSize(ecbEncryptionOracle, *buffer);
+  size_t blockSize = findBlockSize(ecbEncryptionOracle);
   std::vector<uint8_t> testInput(2 * blockSize, 'A');
   std::vector<uint8_t> ciphertext = ecbEncryptionOracle(testInput);
   if (!isECB(ciphertext)) {
@@ -32,6 +30,17 @@ int challenge2_12(int argc, char **argv) {
     return 1;
   }
 
-  std::cout << "ECB mode!" << '\n';
+  std::vector<uint8_t> plaintext;
+  size_t plaintextSize = findPlaintextSize(ecbEncryptionOracle);
+  for (size_t i = 0; i < plaintextSize; ++i) {
+    auto byte =
+        decryptByteAtIndex(ecbEncryptionOracle, blockSize, i, plaintext);
+    if (!byte) {
+      std::cout << "Error: no match found for index " << i << '\n';
+      return 1;
+    }
+    plaintext.push_back(*byte);
+  }
+  std::cout << bytesToString(plaintext) << "\n";
   return 0;
 }
